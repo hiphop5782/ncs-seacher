3c796caa20f3f80c958c26c6f08d5876ec4d0e43
hiphop5782 <zdlsz@hanmail.net>
2022-07-15 09:40:01 +0900

    ui project start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Container, Grid, TextField, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@mui/material';
+import ImageIcon from '@mui/icons-material/Image';
 import './App.css';
 
-function App() {
+function App(){
+  const [keyword, setKeyword] = useState("");
+  const moduleList = [
+    {
+      name:"애플리케이션 설계",
+      main:"20010101_19v4"
+    },
+    {
+      name:"애플리케이션 배포",
+      main:"20010102_19v4"
+    },
+    {
+      name:"애플리케이션 구현",
+      main:"20010103_19v4"
+    },
+  ]
+
+  let filterList = [];
+  const search = ()=>{
+    filterList = moduleList.filter(m=>m.name.indexOf(keyword) >= 0);
+  };
+  const change = (e)=>{
+    setKeyword(e.target.value);
+    search();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Container maxWidth="lg">
+
+    <Grid container marginTop={3}>
+      <Grid item xs={12}>
+        <TextField fullWidth onInput={change}></TextField>
+      </Grid>
+    </Grid>
+
+    <Grid container marginTop={3}>
+      <Grid item xs={12}>
+        '{keyword}' 에 대한 검색 결과 ({filterList.length}개)
+      </Grid>
+    </Grid>
+
+    <Grid container marginTop={3}>
+      <Grid item xs={12}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {filterList.map(m=>(
+            <ListItem key={m.main}>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={m.name} secondary={m.main} />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
+
+  </Container>
+  )
 }
 
 export default App;
